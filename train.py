dab8cd4ca4ce0bbbca1e9807027112d462c9aba5
RAWINAN SUWISUT <6610110259@psu.ac.th>
2024-02-29 17:28:37 +0700

    pot graph

--- train.py
+++ train.py
@@ -2,13 +2,15 @@ import pandas as pd
 from pycaret.regression import *
 
 data = pd.read_csv('cleaned_air4thai.csv')
+data = data.dropna(subset=['PM25'])
+
 
 s = setup(data, target='PM25', session_id=123)
 
-best_model = compare_models()
+best = compare_models()
 
-tuned_model = tune_model(best_model)
+# tuned = tune_model(best)
 
-evaluate_model(tuned_model)
+# evaluate_model(tuned)
 
-predictions = predict_model(tuned_model, data=new_data)
+plot_model(best, plot = 'residuals')
