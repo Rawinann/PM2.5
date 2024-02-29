4a9354ded019c615507e6521ad0a4bb7955ef833
Jaranya Boonthong <faiijaranya0159@gmail.com>
2024-02-29 15:40:14 +0700

    Add Graph

--- wed.py
+++ wed.py
@@ -1,5 +1,5 @@
-# Import packages
-from dash import Dash, html, dash_table
+from dash import Dash, html, dash_table, dcc
+import plotly.express as px
 import pandas as pd
 
 # Incorporate data
@@ -8,10 +8,13 @@ df = pd.read_csv('cleaned_air4thai.csv')
 # Initialize the app
 app = Dash(__name__)
 
-# App layout
+# Check for existence of required columns (optional):
+print(df.columns)  # Uncomment to check available column names
+
 app.layout = html.Div([
     html.Div(children='My First App with Data'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10),
+    dcc.Graph(figure=px.histogram(df, x='PM25', y='TEMP', histfunc='avg'))  # Assuming these columns exist
 ])
 
 # Run the app
