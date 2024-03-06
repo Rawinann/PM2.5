57bc5dd91f02123c2ab4469fb3003ebced1c7ef2
Jaranya Boonthong <faiijaranya0159@gmail.com>
2024-03-06 15:53:29 +0700

    Add HTMl

--- app.py
+++ app.py
@@ -1,37 +1,20 @@
 from dash import Dash, html, dash_table, dcc
 import plotly.express as px
 import pandas as pd
-
+import dash_bootstrap_components as dbc
+import numpy as np
+from dash.dependencies import Output, Input
 
 # Incorporate data
 df = pd.read_csv('cleaned_air4thai.csv')
 
-# def generate_table(dataframe, max_rows=10):
-#     return html.Table([
-#         html.Thead(
-#             html.Tr([html.Th(col) for col in dataframe.columns])
-#         ),
-#         html.Tbody([
-#             html.Tr([
-#                 html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-#             ]) for i in range(min(len(dataframe), max_rows))
-#         ])
-#     ])
-
-# # Initialize the app
-# app = Dash(__name__)
-
-# # Check for existence of required columns (optional):
-# # print(df.columns)  # Uncomment to check available column names
-
-# app.layout = html.Div([
-#     html.Div(children='My Data', style={'textAlign': 'center'}),
-#     dcc.Graph(figure=px.histogram(df, x='PM25', y='TEMP', histfunc='avg')),
-#     dash_table.DataTable(data=df.to_dict('records'), page_size=10),
-#     generate_table(df)
-    
-# ])
-
+external_stylesheets = [
+    {
+        "href": "https://fonts.googleapis.com/css2?"
+        "family=Lato:wght@400;700&display=swap",
+        "rel": "stylesheet",
+    },
+]
 
 def generate_table(dataframe, max_rows=10):
     return dash_table.DataTable(
@@ -41,17 +24,59 @@ def generate_table(dataframe, max_rows=10):
     )
 
 # Initialize the app
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-# Check for existence of required columns (optional):
-# print(df.columns)  # Uncomment to check available column names
+server = app.server
+app.title = "Air Quality Analytics: Understand Your Air Quality!"
 
-app.layout = html.Div([
-    html.Div(children='My Data', style={'textAlign': 'center'}),
-    dcc.Graph(figure=px.histogram(df, x='PM25', y='TEMP', histfunc='avg')),
-    generate_table(df)
-])
+app.layout = html.Div(
+    children=[
+        # ส่วน Header
+        html.Div(
+            children=[
+                html.P(children="🌬️", className="header-emoji"),
+                html.H1(
+                    children="Air Quality Analytics", className="header-title"
+                ),
+                html.P(
+                    children="Analyze the air quality data"
+                    " with various parameters",
+                    className="header-description",
+                ),
+            ],
+            className="header",
+        ),
+        
+        # ส่วน Menu
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        html.Div(children="Station ID", className="menu-title"),
+                        dcc.Dropdown(
+                            id="station-filter",
+                            options=[
+                                {"label": station, "value": station}
+                                for station in np.sort(df['stationID'].unique())
+                            ],
+                            value="44t",
+                            clearable=False,
+                            className="dropdown",
+                        ),
+                    ]
+                ),
+            ],
+            className="menu",
+        ),
 
+        # ส่วน My Data
+        html.Div([
+            html.Div(children='My Data', style={'textAlign': 'center'}),
+            dcc.Graph(figure=px.histogram(df, x='PM25', y='TEMP', histfunc='avg')),
+            generate_table(df)
+        ])
+    ]
+)
 
 # Run the app
 if __name__ == '__main__':
