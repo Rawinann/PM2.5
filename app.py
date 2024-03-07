82ce57f2588b94b8f3d7ce3fec0b11f414629fd5
Jaranya Boonthong <faiijaranya0159@gmail.com>
2024-03-08 02:34:05 +0700

    Test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 from dash.dependencies import Output, Input
 
 # Incorporate data
-df = pd.read_csv('data/predictions.csv')
+df = pd.read_csv('PM2.5/data/predictions.csv')
 
 external_stylesheets = [
     {
